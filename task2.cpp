a089652d8649ce0faf71b42d8841260359870a0d
SriramVarma4 <78374830+SriramVarma4@users.noreply.github.com>
2021-03-07 19:55:58 +0530

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -38,3 +38,23 @@ int main(int argc,char* argv[]){
     pts_dst.push_back(Point2f(400, 800));//right bottom
     pts_dst.push_back(Point2f(400, 0));//right up
 
+    Mat h = findHomography(pts_src, pts_dst);
+    //warped image
+    Mat warp_back;
+    // Warp source image to destination based on homography
+    warpPerspective(back_image, warp_back, h, back_image.size());
+    // Display image
+    //Mat im_out = Mat(900,1300,0);
+	Mat back_crop = warp_back(Rect(0, 0, 400, 800));
+	//imshow("bc", back_crop);
+	int fm_count = 1;
+	int rows = back_crop.rows;
+	int cols = back_crop.cols;
+
+	Mat f_crop;
+	cout<<"executing\n";
+	//new
+	Ptr<BackgroundSubtractor> pBackSub;
+	pBackSub = createBackgroundSubtractorMOG2();
+	Mat fgmask;
+	Mat frame;
