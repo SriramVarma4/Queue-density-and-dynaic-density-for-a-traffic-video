0009738c97b5102383a1c92fe2641806ecff0bb4
gbnm2001 <78373993+gbnm2001@users.noreply.github.com>
2021-03-07 19:55:04 +0530

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -18,3 +18,23 @@ int main(int argc,char* argv[]){
 		cout<<"\nerror reading the video\n";
 		return 0;
 	}
+	Mat imgdiff;
+	//Mat back_image = imread("empty.png", IMREAD_GRAYSCALE);
+    Mat back_image = imread("empty.png");
+    if ( back_image.empty() ) 
+    { 
+        cout << "Error loading the image" << endl;
+        return -1; 
+    }
+    vector<Point2f> pts_src;
+    pts_src.push_back(Point2f(925, 220));//left up
+    pts_src.push_back(Point2f(225, 1004));//left bottom
+    pts_src.push_back(Point2f(1552, 1004));//right bottom
+    pts_src.push_back(Point2f(1295, 220));//right up
+
+    vector<Point2f> pts_dst;
+    pts_dst.push_back(Point2f(0, 0));//left up
+    pts_dst.push_back(Point2f(0, 800));//left bottom
+    pts_dst.push_back(Point2f(400, 800));//right bottom
+    pts_dst.push_back(Point2f(400, 0));//right up
+
