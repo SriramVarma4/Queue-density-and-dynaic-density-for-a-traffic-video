50a19adc57828e05c98d6c4f8968b9aac8ca69a6
SriramVarma4 <78374830+SriramVarma4@users.noreply.github.com>
2021-03-07 19:54:00 +0530

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+//#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include "opencv2/opencv.hpp"
 
+using namespace std;
+using namespace cv;
+
+int main(int argc,char* argv[]){
+	VideoCapture cap(argv[1]);
+	if (!cap.isOpened()){
+		cout<<"\nerror reading the video\n";
+		return 0;
+	}
